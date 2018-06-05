74af2499cd5b8b96abf6903ccda7348fa01a5062
jlivsey <james.a.livsey@gmail.com>
2018-06-05 13:58:32 -0400

    clean up

--- R/list_package_functions.R
+++ R/list_package_functions.R
@@ -1,1 +1,15 @@
-ls("package:sigex")
+#' List all functions exported when loading a package
+#'
+#' @param package.name characer string giving the name of the package
+#'
+#' @return character vector with names of functions exported
+#' @export
+#'
+
+list_functions <- function(package.name)
+{
+  library(package.name)
+  str = paste("package:", package.name, sep="")
+  return(ls(str))
+}
+
