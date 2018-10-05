40a729a517db3dd1ab931872c40e7b65a571411c
Jim Livsey <james.a.livsey@gmail.com>
2018-10-05 14:49:31 -0400

    upd matrix_head

--- R/matrix_head.R
+++ R/matrix_head.R
@@ -4,11 +4,16 @@
 #'
 #' @param M matrix
 #' @param n number of rows/cols to print (default is 10)
+#' @param round specify an integer to pass to the round function
 #'
 #' @return prints M[1:n, 1:n]
 #' @export
 #'
 
-matrix_head = function(M, n=10){
-  print(M[1:10, 1:10])
+matrix_head = function(M, n=10, round=NA){
+  if(is.na(round)){
+    print(M[1:n, 1:n])
+  }else{
+
+  }
 }
