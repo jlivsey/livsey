39dbd374679519539b0834e003b0bf3e9f218865
Jim Livsey <james.a.livsey@gmail.com>
2018-10-05 14:55:12 -0400

    upd matrix_head function again

--- R/matrix_head.R
+++ R/matrix_head.R
@@ -10,10 +10,10 @@
 #' @export
 #'
 
-matrix_head = function(M, n=10, round=NA){
-  if(is.na(round)){
+matrix_head = function(M, n=10, round_integer=NA){
+  if(is.na(round_integer)){
     print(M[1:n, 1:n])
   }else{
-
+    print(round(M[1:n, 1:n], round_integer))
   }
 }
