e41954237cb9365a16a8bdf73ad255510cf22ae3
J. Livsey <james.a.livsey@gmail.com>
2019-05-07 08:46:07 -0400

    added standardize()

---
+++ R/standardize.R
@@ -0,0 +1,14 @@
+#' Subtract mean and divide by standard deviation
+#'
+#' @param x vector to be standardized
+#'
+#' @return vector of same length of x with mean subtracted and divided 
+#'             through by the variance
+#' @export
+#'
+standardize <- function(x){
+  
+  if(!is.numeric(x)) stop('input is not of type numeric')
+  
+  (x - mean(x)) / sd(x)
+}
