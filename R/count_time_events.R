247969f514166d4440f2a477ce664d79bdffe6f1
J. Livsey <james.a.livsey@gmail.com>
2019-09-13 11:00:37 -0400

    add count_time_events function

---
+++ R/count_time_events.R
@@ -0,0 +1,55 @@
+
+
+#' Count time-stamped events
+#'
+#' @param event_dates vector of class "Date" of events to be counts
+#' @param event_type 'year', 'month', 'week', 'day', 'hour' or 'minute'
+#' @param event_all_possible (optional) specify all possible dates to count
+#'
+#' @return table of counts for each event by event_type
+#' @export
+#'
+count_time_events <- function(event_dates,
+                              event_type =  c('year',
+                                              'month',
+                                              'week',
+                                              'day',
+                                              'hour',
+                                              'minute'),
+                              event_all_possible = NULL
+                              ){
+
+    if(class(event_dates) != "Date"){
+      stop("event_dates must be vector of class Date")
+    }
+   '%!in%' <- function(x,y){ # hardcode "not in" function
+      !('%in%'(x,y))
+    }
+    all.event.types <- c('year',
+                         'month',
+                         'week',
+                         'day',
+                         'hour',
+                         'minute'
+                         )
+    if(event_type %!in% all.event.types){
+      stop("event_type must be year, month, week, day, hour or minute")
+    }
+
+    events <- floor_date(event_dates, event_type)
+
+    if(is.null(event_all_possible)){
+      first <- sort(event_dates)[1]
+      last  <- sort(event_dates)[length(event_dates)]
+      event_all_possible <- seq(from = first,
+                                to = last,
+                                by = event_type)
+    }
+
+    tab <- table(factor(event_dates,
+                        levels = factor(event_all_possible)))
+
+    return(tab)
+
+
+} # end function
