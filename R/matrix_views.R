702394737c8f3e259a99bb84225507b368c8aad3
J. Livsey <james.a.livsey@gmail.com>
2021-09-20 11:15:44 -0400

    add and document matrix_views funcs

---
+++ R/matrix_views.R
@@ -0,0 +1,69 @@
+#' Print topleft of matrix
+#'
+#' @param M matrix to view
+#' @param nView Number of rows/cols to view
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' M <- diag(1:12)
+#' topleft(M)
+#' topleft(M, 3)
+topleft <- function(M, nView = 5){
+  print(M[1:nView, 1:nView])
+}
+
+#' Print topright of matrix
+#'
+#' @param M matrix to view
+#' @param nView Number of rows/cols to view
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' M <- diag(1:12)
+#' topright(M)
+#' topright(M, 3)
+topright <- function(M, nView = 5){
+  n <- nrow(M)
+  m <- ncol(M)
+  print(M[1:nView, (m+1-nView):m])
+}
+
+#' Print bottomleft of matrix
+#'
+#' @param M matrix to view
+#' @param nView Number of rows/cols to view
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' M <- diag(1:12)
+#' bottomleft(M)
+#' bottomleft(M, 3)
+bottomright <- function(M, nView = 5){
+  n <- nrow(M)
+  m <- ncol(M)
+  print(M[(n+1-nView):n, (m+1-nView):m])
+}
+
+#' Print bottom-right of matrix
+#'
+#' @param M matrix to view
+#' @param nView Number of rows/cols to view
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' M <- diag(1:12)
+#' bottomright(M)
+#' bottomright(M, 3)
+bottomleft <- function(M, nView = 5){
+  n <- nrow(M)
+  m <- ncol(M)
+  print(M[(n+1-nView):n, (m+1-nView):m])
+}
